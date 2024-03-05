b59a5f606816e5855b8603673a18250ed74e60f9
feng6917 <82997695+feng6917@users.noreply.github.com>
2024-03-05 10:03:03 +0800

    Update float.go

--- languages/golang/util/convertor/float.go
+++ languages/golang/util/convertor/float.go
@@ -3,6 +3,7 @@ package convetor
 import (
 	"fmt"
 	"strconv"
+	"encoding/binary"
 )
 
 func Float64ToString(f float64) string {
@@ -24,4 +25,38 @@ func Float64Rand(v float64, dig int) float64 {
 // 浮点数串化(左边是整数位置,右边是小数位,dig参数控制)
 func FloatToFDig(floVal float64, dig int) string {
 	return fmt.Sprintf("%10."+strconv.Itoa(dig)+"f", floVal) //十位整数，8位小数
+}
+
+// byte转浮点数数组
+func ByteToFloat32Array(bf []byte, featureSize int) []float32 {
+	feature := make([]float32, featureSize)
+	for i := 0; i < featureSize; i++ {
+		off := i * 4
+		feature[i] = byteToFloat32(bf[off : off+4])
+	}
+	return feature
+}
+
+func byteToFloat32(bytes []byte) float32 {
+	bits := binary.LittleEndian.Uint32(bytes)
+	return math.Float32frombits(bits)
+}
+
+
+func Float32ToByte(float float32) []byte {
+	bits := math.Float32bits(float)
+	bytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bytes, bits)
+
+	return bytes
+}
+
+// Float32ArrayToByte
+func Float32ArrayToByte(fs []float32) []byte {
+	buf := new(bytes.Buffer)
+	for i := 0; i < len(fs); i++ {
+		tmp := Float32ToByte(fs[i])
+		buf.Write(tmp)
+	}
+	return buf.Bytes()
 }
