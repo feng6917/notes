6d793c8e54e10371d4144292d2b6315a6ed2100c
feng6917 <feng6917@gmail.com>
2024-06-28 21:35:29 +0800

    fix:doc

--- languages/golang/test/doc/tree/tree.go
+++ languages/golang/test/doc/tree/tree.go
@@ -8,6 +8,9 @@ import (
 /*
 	今天跟同事聊起来后端返回前端树的问题，不考虑大数据量情况，一般数据量情况下如何更好的维护
 	简单的写一个小例子进行测试，实际工作中需要针对数据进行分析，是否频繁操作，父子级哪个更适合用于key，是否针对一定量级特殊处理，分页等
+	对于一个树需要考虑很多东西，广度，深度（大多数不需要考虑），假设场景，广度可以通过子级列表判断，深度可以通过level来判断
+	考虑到节点数据检索，是否应该把该层级数据放到数据库中处理
+	当一个层级数据几百以上时，就要首先考虑需求得合理性，用户能看这么多数据么，直接进行检索不香么
 	具体情况具体分析，没有什么是万能的解决方式，不断改进，不断优化。
 */
 
@@ -21,6 +24,56 @@ type Node struct {
 type CacheTree struct {
 	Level sync.Map   // 层级映射 存储层级映射关系
 	Data  []sync.Map // 数据映射 层级数据存储切片
+}
+
+// autoRemoveNoParentIDNode
+func (c *CacheTree) autoRemoveNoParentIDNode() {
+	// 倒序处理
+	for i := len(c.Data); i > 0; i-- {
+		fmt.Println(i)
+		c.removeCurrentNode(i)
+	}
+}
+
+func (c *CacheTree) removeCurrentNode(l int) {
+	if l-1 < 1 {
+		return
+	}
+	// 获取父级ID列表
+	m := make(map[uint32]struct{})
+	c.Data[l-1].Range(func(key, value interface{}) bool {
+		k, ok := key.(uint32)
+		if ok {
+			m[k] = struct{}{}
+		}
+		return true
+	})
+
+	var nodes []Node
+	// 匹配当前级
+	c.Data[l-1].Range(func(key, value interface{}) bool {
+		val, ok := value.(Node)
+		if ok {
+			_, exist := m[val.ParentID]
+			if !exist {
+				nodes = append(nodes, val)
+			}
+		}
+		return true
+	})
+
+	// 移除无关联数据
+	c.removeNodes(nodes)
+
+}
+
+func (c *CacheTree) removeNodes(nodes []Node) {
+	if len(nodes) == 0 {
+		return
+	}
+	for _, node := range nodes {
+		c.Data[node.Level].Delete(node.ID)
+	}
 }
 
 // upInsertLevel 插入更新等级（同时创建空数据）
@@ -51,6 +104,12 @@ func (c *CacheTree) UpInsert(n Node) {
 	c.upInsertLevel(n.Level)
 	// 数据存储
 	c.storeData(n)
+}
+
+// Remove 移除
+func (c *CacheTree) Remove(l int, id uint32) {
+	// 仅移除此节点 子级节点定时自动清理
+	c.Data[l].Delete(id)
 }
 
 // 数结构
@@ -125,6 +184,25 @@ func main() {
 	ct.UpInsert(n2)
 	ct.UpInsert(n3)
 
-	ts := ct.GetTree(0, 0)
-	fmt.Println(ts)
+	// ts := ct.GetTree(0, 0)
+	// fmt.Println(ts)
+
+	for _, k := range ct.Data {
+		k.Range(func(key, value interface{}) bool {
+			fmt.Println("remove before: ", key, value)
+			return true
+		})
+	}
+
+	ct.Remove(1, 2)
+	ct.autoRemoveNoParentIDNode()
+
+	// ts1 := ct.GetTree(0, 0)
+	// fmt.Println(ts1)
+	for _, k := range ct.Data {
+		k.Range(func(key, value interface{}) bool {
+			fmt.Println("remove after: ", key, value)
+			return true
+		})
+	}
 }
