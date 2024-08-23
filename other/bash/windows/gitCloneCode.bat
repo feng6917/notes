0de49b25ca8fefafc4a8c24576b92da6447c0c72
feng6917 <feng6917@gmail.com>
2024-08-23 16:40:45 +0800

    feat: any life

--- other/bash/windows/gitCloneCode.bat
+++
@@ -1,8 +0,0 @@
-echo "清理存储文件夹"
-cd C:/Users/Administrator/Desktop/test/
-rd /s /q "ar_device_core"
-echo "清理存储文件夹完成"
-
-echo "克隆代码"
-"E:/app install/Git/bin/git.exe" clone https://git.hiscene.net/hifoundry/micro-server/ar-device-core C:/Users/Administrator/Desktop/test/ar_device_core
-echo "克隆代码完成"
